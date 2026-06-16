1ab495b5197ed6f5464b36f417a0b9acfe56c7c3
Toby Nguyen <tobynguyen27@proton.me>
2026-06-16 21:50:18 +0700

    chore(no-react-props): update test cases

--- src/tests/noReactProps.test.ts
+++ src/tests/noReactProps.test.ts
@@ -4,17 +4,54 @@ import { noReactProps } from '@rules/noReactProps';
 ruleTester.run('no-react-props', noReactProps, {
 	valid: [
 		{
-			name: 'Good practice',
-			code: `<MyReactComponent class="foo" for="#password" />;`,
+			name: 'Uses "class" instead of "className"',
+			code: `<div class="foo" />`,
+		},
+		{
+			name: 'Uses "for" instead of "htmlFor"',
+			code: `<label for="name">Name</label>`,
+		},
+		{
+			name: 'Unrelated props are allowed',
+			code: `<input type="text" id="name" />`,
+		},
+		{
+			name: 'Spread attributes are ignored',
+			code: `<div {...props} className="foo" />`,
+		},
+		{
+			name: 'Component (uppercase) with className is ignored',
+			code: `<MyComponent className="foo" />`,
+		},
+		{
+			name: 'JSXMemberExpression with className is ignored',
+			code: `<Foo.Bar className="foo" />`,
 		},
 	],
-
 	invalid: [
 		{
-			name: 'Bad practice',
-			code: `<MyReactComponent className="foo" htmlFor="#password" />;`,
-			output: `<MyReactComponent class="foo" for="#password" />;`,
-			errors: [{ messageId: 'prefer' }, { messageId: 'prefer' }],
+			name: 'className should be class',
+			code: `<div className="foo" />`,
+			errors: [{ messageId: 'preferClass' }],
+			output: `<div class="foo" />`,
+		},
+		{
+			name: 'htmlFor should be for',
+			code: `<label htmlFor="name">Name</label>`,
+			errors: [{ messageId: 'preferFor' }],
+			output: `<label for="name">Name</label>`,
+		},
+		{
+			name: 'Both className and htmlFor reported',
+			code: `<label className="foo" htmlFor="name">Name</label>`,
+			errors: [{ messageId: 'preferClass' }, { messageId: 'preferFor' }],
+			output: `<label class="foo" for="name">Name</label>`,
+		},
+		{
+			name: 'className on a native element with other valid props',
+			code: `<input type="text" className="bar" />`,
+			errors: [{ messageId: 'preferClass' }],
+			output: `<input type="text" class="bar" />`,
 		},
 	],
 });
