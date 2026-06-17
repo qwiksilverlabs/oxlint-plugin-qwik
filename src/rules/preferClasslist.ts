5264995e6c972ac0b6a5787c8f0be89be7de5e41
Toby Nguyen <tobynguyen27@proton.me>
2026-06-17 15:48:04 +0700

    refactor(prefer-classlist):

--- src/rules/preferClasslist.ts
+++ src/rules/preferClasslist.ts
@@ -1,15 +1,22 @@
 import { defineRule, type Rule } from '@oxlint/plugins';
-import { propName } from '../utils';
+
+const DEFAULT_CLASSNAMES = ['cn', 'clsx', 'classnames'] as const;
 
 export const preferClasslist: Rule = defineRule({
 	meta: {
-		type: 'problem',
+		type: 'suggestion',
 		docs: {
-			recommended: false,
 			description:
 				'Enforce using the classlist prop over importing a classnames helper. The classlist prop accepts an object `{ [class: string]: boolean }` just like classnames.',
+			recommended: false,
 			url: 'https://qwik.dev/docs/advanced/eslint/#prefer-classlist',
 		},
+		messages: {
+			preferClasslist:
+				'The classlist prop should be used instead of {{ classnames }} to efficiently set classes based on an object.',
+		},
+		fixable: undefined,
+		hasSuggestions: false,
 		schema: [
 			{
 				type: 'object',
@@ -18,7 +25,7 @@ export const preferClasslist: Rule = defineRule({
 						type: 'array',
 
 						description: 'An array of names to treat as `classnames` functions',
-						default: ['cn', 'clsx', 'classnames'],
+						default: [...DEFAULT_CLASSNAMES],
 						items: {
 							type: 'string',
 							minItems: 1,
@@ -29,44 +36,36 @@ export const preferClasslist: Rule = defineRule({
 				additionalProperties: false,
 			},
 		],
-		messages: {
-			preferClasslist:
-				'The classlist prop should be used instead of {{ classnames }} to efficiently set classes based on an object.',
-		},
+		defaultOptions: [{ classnames: [...DEFAULT_CLASSNAMES] }],
+		deprecated: false,
 	},
-	create(context) {
-		const modifyJsxSource = context.sourceCode
-			.getAllComments()
-			.some((c) => c.value.includes('@jsxImportSource'));
-		if (modifyJsxSource) {
-			return {};
-		}
-
-		const classnames = (context.options?.[0] as { classnames?: string[] } | undefined)
-			?.classnames ?? ['cn', 'clsx', 'classnames'];
-
+	createOnce(context) {
 		return {
 			JSXAttribute(node) {
-				if (['class', 'className'].indexOf(propName(node)) === -1) {
-					return;
-				}
+				const { classnames } = (context.options?.[0] as
+					| { classnames: string[] }
+					| undefined) ?? { classnames: [...DEFAULT_CLASSNAMES] };
 
-				if (node.value?.type === 'JSXExpressionContainer') {
-					const expr = node.value.expression;
+				const name =
+					node.name.type === 'JSXIdentifier' ? node.name.name : node.name.namespace.name;
 
-					if (
-						expr.type === 'CallExpression' &&
-						expr.callee.type === 'Identifier' &&
-						classnames.indexOf(expr.callee.name) !== -1
-					) {
-						context.report({
-							node,
-							messageId: 'preferClasslist',
-							data: {
-								classnames: expr.callee.name,
-							},
-						});
-					}
+				if (name !== 'class') return;
+				if (node.value?.type !== 'JSXExpressionContainer') return;
+
+				const { expression } = node.value;
+
+				if (
+					expression.type === 'CallExpression' &&
+					expression.callee.type === 'Identifier' &&
+					classnames.includes(expression.callee.name)
+				) {
+					context.report({
+						node,
+						messageId: 'preferClasslist',
+						data: {
+							classnames: expression.callee.name,
+						},
+					});
 				}
 			},
 		};
