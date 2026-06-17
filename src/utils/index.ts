792e60a83aaf752de2584b9349897795c8783e77
Toby Nguyen <tobynguyen27@proton.me>
2026-06-17 15:59:31 +0700

    chore: remove unused code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,1 @@
-export { propName } from './propName';
 export { getProgramBody } from './getProgramBody';
--- src/utils/propName.ts
+++
@@ -1,8 +0,0 @@
-import { type ESTree } from '@oxlint/plugins';
-
-export function propName(node: ESTree.JSXAttribute): string {
-	if (node.name.type === 'JSXNamespacedName')
-		return `${node.name.namespace.name}:${node.name.name.name}`;
-
-	return node.name.name;
-}
